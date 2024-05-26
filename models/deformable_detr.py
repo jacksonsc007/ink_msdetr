f213b6de0d30f08869418baa70852ed578a5cb56
jackson <jacksonsc@whut.edu.cn>
2024-05-26 21:44:34 +0800

    v1.0.2

--- models/deformable_detr.py
+++ models/deformable_detr.py
@@ -32,7 +32,6 @@ from .deformable_transformer import build_deforamble_transformer
 
 from .matcher_o2m import Stage2Assigner
 
-
 def _get_clones(module, N):
     return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
 
