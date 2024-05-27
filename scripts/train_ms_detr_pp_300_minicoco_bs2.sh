bd484b02695c06c888451e9117334bad017ddae5
jackson <jacksonsc@whut.edu.cn>
2024-05-27 17:35:42 +0800

    use shortersize_480 image size setting

--- scripts/train_ms_detr_pp_300_minicoco_bs2.sh
+++ scripts/train_ms_detr_pp_300_minicoco_bs2.sh
@@ -5,14 +5,20 @@ num_enc_layers=6
 num_dec_layers=6
 dataset=minicoco
 batch_size=2
+device_code=4090
+backbone=resnet18
+num_queries=100
 
-EXP_DIR=exps/${dataset}-cascade-msdetr_enc${num_enc_layers}_dec${num_dec_layers}-bs${batch_size}
+exp_code=shortersize_480-${device_code}_${dataset}-cascade-msdetr_baseline_${backbone}_enc${num_enc_layers}_dec${num_dec_layers}_query${num_queries}-bs${batch_size}
+EXP_DIR=exps/${exp_code}
 
 mkdir -p $EXP_DIR
 
 GPUS_PER_NODE=$num_gpus ./tools/run_dist_launch.sh $num_gpus python -u main.py \
    --lr 2.5e-5 \
    --lr_backbone 2.5e-6 \
+   --wandb_enabled \
+   --wandb_name $exp_code \
    --batch_size $batch_size \
    --enc_layers $num_enc_layers \
    --dec_layers $num_dec_layers \
@@ -23,7 +29,7 @@ GPUS_PER_NODE=$num_gpus ./tools/run_dist_launch.sh $num_gpus python -u main.py \
    --epochs 12 \
    --lr_drop 11 \
    --coco_path=$coco_path \
-   --num_queries 300 \
+   --num_queries $num_queries \
    --dropout 0.0 \
    --mixed_selection \
    --look_forward_twice \
